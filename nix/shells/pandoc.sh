ec95db547fcd3d5b13942021f61b42ce885a151b
guilherme-n-l <acc.guilhermenl@gmail.com>
2025-07-23 21:12:25 -0300

    feat: Pandoc shell

--- nix/shells/pandoc.sh
+++ nix/shells/pandoc.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo "Welcome to pandoc environment. Use \`help\` for a list of commands"
+
+ENVIRON=pandoc
+
+echo "Welcome to $ENVIRON environment. Use \`help\` for a list of commands"
 
 help() {
     declare -A functions=(
         ["topdf"]="Convert a file to pdf -> topdf [FILE] [ARGS...]"
     )
 
-    echo "pandoc environment available commands:"
+    echo "$ENVIRON environment available commands:"
 
     for cmd in "${!functions[@]}"; do
         echo -e "\t$cmd:\t${functions[$cmd]}"
