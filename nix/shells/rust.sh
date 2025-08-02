207788f63a6a052c1338f6cfc712c6d64d115ed6
guilherme-n-l <acc.guilhermenl@gmail.com>
2025-08-02 19:02:14 -0300

    feat: Rust shell

---
+++ nix/shells/rust.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ENVIRON=rust
+
+echo "Welcome to $ENVIRON environment. Use \`help\` for a list of commands"
+for cmd in rustc cargo rust-analyzer rustfmt; do
+    $cmd --version
+done
+
+help() {
+    declare -A functions=()
+
+    echo "$ENVIRON environment available commands:"
+
+    for cmd in "${!functions[@]}"; do
+        echo -e "\t$cmd:\t${functions[$cmd]}"
+    done
+}
