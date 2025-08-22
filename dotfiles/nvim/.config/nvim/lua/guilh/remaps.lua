64f2591c1e2f3f876fe6cac89b4479e83168cc20
guilherme-n-l <acc.guilhermenl@gmail.com>
2025-08-22 09:21:09 -0300

    fix: harpoon binds

--- dotfiles/nvim/.config/nvim/lua/guilh/remaps.lua
+++ dotfiles/nvim/.config/nvim/lua/guilh/remaps.lua
@@ -1,6 +1,7 @@
 local trouble = require("trouble")
 local harpoonui = require("harpoon.ui")
 local harpoon = require("harpoon.mark")
+local harpoon_goto = harpoonui.nav_file
 local conform = require("conform")
 local utils = require("guilh.utils")
 
@@ -11,16 +12,6 @@ local globals = {
 
 for k, v in pairs(globals) do
 	vim.g[k] = v
-end
-
-local harpoon_nav_binds = {}
-for i = 1, 9 do
-	table.insert(harpoon_nav_binds, {
-		"<leader>" .. i,
-		function()
-			harpoonui.nav_file(i)
-		end,
-	})
 end
 
 local mappings = {
@@ -40,7 +31,60 @@ local mappings = {
 		{ "<leader>hn", harpoonui.nav_next },
 		{ "<leader>hp", harpoonui.nav_prev },
 		{ "<leader>hv", harpoonui.toggle_quick_menu },
-		unpack(harpoon_nav_binds),
+		{
+			"<leader>1",
+			function()
+				harpoon_goto(1)
+			end,
+		},
+		{
+			"<leader>2",
+			function()
+				harpoon_goto(2)
+			end,
+		},
+		{
+			"<leader>3",
+			function()
+				harpoon_goto(3)
+			end,
+		},
+		{
+			"<leader>4",
+			function()
+				harpoon_goto(4)
+			end,
+		},
+		{
+			"<leader>5",
+			function()
+				harpoon_goto(5)
+			end,
+		},
+		{
+			"<leader>6",
+			function()
+				harpoon_goto(6)
+			end,
+		},
+		{
+			"<leader>7",
+			function()
+				harpoon_goto(7)
+			end,
+		},
+		{
+			"<leader>8",
+			function()
+				harpoon_goto(8)
+			end,
+		},
+		{
+			"<leader>9",
+			function()
+				harpoon_goto(9)
+			end,
+		},
 
 		{ "J", "mzJ`z" },
 		{ "<C-d>", "<C-d>zz" },
