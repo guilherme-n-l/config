8b566e10953de50ade478e1b18b44f60b14831e3
guilherme-n-l <acc.guilhermenl@gmail.com>
2025-08-27 10:13:30 -0300

    feat: nixflake refactoring

--- nix/modules/shared/packages.nix
+++ nix/modules/shared/packages.nix
@@ -1,26 +1,29 @@
-{pkgs}: {
-  pkgs = with pkgs; [
-    ffmpeg
-    neovim
-    git
-    lazygit
-    curl
-    wget
-    yazi
-    fzf
-    fd
-    stow
-    tree
-    ripgrep
-    zoxide
-    tealdeer
+{
+  inputs,
+  pkgs,
+}:
+(with pkgs; [
+  ffmpeg
+  neovim
+  git
+  lazygit
+  curl
+  wget
+  yazi
+  fzf
+  fd
+  stow
+  tree
+  ripgrep
+  zoxide
+  tealdeer
 
-    # LUA LSP
-    luajitPackages.lua-lsp
-    stylua
+  # LUA LSP
+  luajitPackages.lua-lsp
+  stylua
 
-    # NIX LSP
-    nil
-    alejandra
-  ];
-}
+  # NIX LSP
+  nil
+  alejandra
+])
+++ (with inputs; [yap.packages.${pkgs.system}.default])
