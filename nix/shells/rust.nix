8461f1931a1fa63e503d2df170d47c79b8edc0cb
guilherme-n-l <acc.guilhermenl@gmail.com>
2025-09-18 18:50:41 -0300

    feat: add clippy

--- nix/shells/rust.nix
+++ nix/shells/rust.nix
@@ -1,5 +1,5 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
-  buildInputs = with pkgs; [rustc rust-analyzer rustfmt cargo];
+  buildInputs = with pkgs; [rustc rust-analyzer rustfmt cargo clippy];
   shellHook = builtins.readFile ./rust.sh;
 }
