116b0cc21b0a84d4310f6f75242466436b29153a
guilherme-n-l <acc.guilhermenl@gmail.com>
2026-05-26 15:49:05 -0300

    feat(air): space motion shortcuts

--- ezConfigs/darwin/configurations/air.nix
+++ ezConfigs/darwin/configurations/air.nix
@@ -45,6 +45,41 @@ in
         "com.apple.HIToolbox"."AppleFnUsageType" = 0; # Disable fn Lock
         "com.apple.dock"."show-recents" = false; # Disable recentes in dock
         "com.apple.finder"."CreateDesktop" = false; # Hide desktop icons
+        "com.apple.symbolichotkeys"."AppleSymbolicHotKeys" = {
+          # "xx" = {
+          #   enabled = true;
+          #   value = {
+          #     parameters = [ <char-code>  <key-code>  <modifier-flags> ];
+          #     type = "standard";
+          #   };
+          # };
+
+          # Move to previous space
+          "79" = {
+            enabled = true;
+            value = {
+              parameters = [
+                49
+                18
+                1048576
+              ]; # cmd+1
+              type = "standard";
+            };
+          };
+          # Move to next space
+          "81" = {
+            enabled = true;
+            value = {
+              parameters = [
+                50
+                19
+                1048576
+              ]; # cmd+2
+              type = "standard";
+            };
+          };
+        };
+
         "pbs"."NSServicesStatus" = {
           "(null) - Open Finder - runWorkflowAsService" = {
             enabled_context_menu = true;
@@ -81,7 +116,7 @@ in
     ];
     casks = [
       # Web
-      "librewolf"
+      "firefox"
 
       # Productivity
       "libreoffice"
@@ -101,7 +136,6 @@ in
       # AI
       "claude-code"
       "codex"
-      "antigravity"
     ];
   };
 
