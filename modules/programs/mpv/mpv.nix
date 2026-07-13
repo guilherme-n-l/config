1cdd6e91acccb172aba63de5304150e4ab9073da
guilherme-n-l <acc.guilhermenl@gmail.com>
2026-07-12 22:04:19 -0300

    fix(mpv): config is now applied for aarch64 .app

--- modules/programs/mpv/mpv.nix
+++ modules/programs/mpv/mpv.nix
@@ -1,14 +1,36 @@
 { self, ... }:
 {
   perSystem =
-    { wrapperPkgs, ... }:
+    { pkgs, wrapperPkgs, ... }:
     let
-      mpv = self.inputs.wrappers.wrappers.mpv.apply { pkgs = wrapperPkgs; };
-    in
-    {
-      packages.mpv = mpv.wrap {
+      mpv = self.inputs.wrappers.wrappers.mpv.apply {
+        pkgs = wrapperPkgs;
+      };
+      wrappedMpv = mpv.wrap {
         "mpv.conf".path = ./mpv.conf;
         "mpv.conf".content = "";
       };
+      darwinMpv =
+        pkgs.runCommand "mpv-wrapped"
+          {
+            nativeBuildInputs = [
+              pkgs.makeWrapper
+              pkgs.makeBinaryWrapper
+            ];
+          }
+          ''
+            cp -R ${wrappedMpv} "$out"
+            chmod -R u+w "$out"
+            makeWrapper ${wrapperPkgs.mpv}/bin/mpv "$out/bin/mpv" \
+              --add-flags "--include=${./mpv.conf}"
+            mv "$out/Applications/mpv.app/Contents/MacOS/mpv" \
+              "$out/Applications/mpv.app/Contents/MacOS/.mpv-wrapped"
+            makeBinaryWrapper "$out/Applications/mpv.app/Contents/MacOS/.mpv-wrapped" \
+              "$out/Applications/mpv.app/Contents/MacOS/mpv" \
+              --add-flags "--include=${./mpv.conf}"
+          '';
+    in
+    {
+      packages.mpv = if pkgs.stdenv.isDarwin then darwinMpv else wrappedMpv;
     };
 }
